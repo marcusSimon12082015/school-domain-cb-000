468c81d83a1a73c5250e824b401d9bb324a089b3
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-31 17:13:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,7 @@ class School
   end
   def add_student(name,number)
     @roster[number] = []
-    @roster[number] << name
-    puts "#{@roster[number][0]}"
-    puts "#{@roster[number].length}"
+    @roster[number].unshift(name)
+    puts "#{@roster[number].inspect}"
   end
 end

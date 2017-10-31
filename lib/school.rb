b03238055002c74a3e9843831f4e753bb1c7e825
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-31 18:58:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(name,grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  def grade(number)
+    @roster[number]
+  end
+  def sort
+    sorted = {}
+    @roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+end
